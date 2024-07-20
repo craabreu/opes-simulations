dad79d05ea021d952298674fc10680ca60015cf4
Charlles Abreu <charlles@redesignscience.com>
2024-07-20 12:19:25 -0400

    Fixed bash script

--- müller-brown/run_all.sh
+++ müller-brown/run_all.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-python run.py --method opes --varfreq 50 --incomingbw
-python run.py --method opes --varfreq 50 --uncompressed
-python run.py --method opes-explore --varfreq 50 --unreweighted
-python run.py --method opes-explore --varfreq 50
-python run.py --method opes-explore --varfreq 50 --uncompressed
-python run.py --method opes-explore --varfreq 50 --unreweighted --uncorrected
-python run.py --method opes-explore --varfreq 50 --bounded
-python run.py --method opes-explore --varfreq 50 --bounded --unreweighted
-python run.py --method opes-explore --varfreq 50 --bounded --unreweighted --uncorrected
-python run.py --method opes-explore --varfreq 50 --incomingbw
+python run.py --method opes --incomingbw
+python run.py --method opes --uncompressed
+python run.py --method opes-explore --unreweighted
+python run.py --method opes-explore
+python run.py --method opes-explore --uncompressed
+python run.py --method opes-explore --unreweighted --uncorrected
+python run.py --method opes-explore --bounded
+python run.py --method opes-explore --bounded --unreweighted
+python run.py --method opes-explore --bounded --unreweighted --uncorrected
+python run.py --method opes-explore --incomingbw
 python run.py --method metad
-python run.py --method opes --varfreq 50
-python run.py --method opes --varfreq 50 --bounded
+python run.py --method opes
+python run.py --method opes --bounded
